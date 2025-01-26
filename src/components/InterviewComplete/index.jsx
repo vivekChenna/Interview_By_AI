5afb19801cdd5975f52225f8804ae2b2439090e0
vivekChenna <chennavivek96@gmail.com>
2025-01-26 18:30:48 +0530

    removed bg-gray

--- src/components/InterviewComplete/index.jsx
+++ src/components/InterviewComplete/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const InterviewComplete = () => {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-[500px] bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-[500px]">
       <div className="p-8 bg-white shadow-lg rounded-2xl text-center">
         <h1 className="text-4xl font-bold mb-4">Your Interview is Over</h1>
         <p className="text-lg mb-6">
